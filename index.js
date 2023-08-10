6b97af0d4fcaa0818013347d7746d1b138977ac3
meganm672 <meganm672@gmail.com>
2023-08-09 21:19:17 -0400

    cleaned up coffee_data and created index file with pseudocode

---
+++ index.js
@@ -0,0 +1,15 @@
+const coffeeMenu = require("./coffee_data");
+
+// print an array of all the drinks on the menu
+
+//print an array of drinks that cost 5 and under
+
+//print an array pf drinks that are priced an even number
+
+//print the total if you were to order one of every drink
+
+//print an array with all the drinks that are seasonal
+
+//Print all the seasonal drinks with 
+//the words "with imported beans" after the item name.
+// For example: "affogato with imported beans".
