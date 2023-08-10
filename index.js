ebbc3d0be04777976b9e3cd6182a3da6f480b232
meganm672 <meganm672@gmail.com>
2023-08-10 11:14:20 -0400

    solved problems 6 and 7 and fixed problem 2 to go into an array

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const coffeeMenu = require("./coffee_data");
 
 // print an array of all the drinks on the menu
-coffeeMenu.forEach(drinks => console.log(drinks));
+const drinks = [];
+coffeeMenu.forEach((drink) => drinks.push(drink));
+console.table(drinks);
 
 //print an array of drinks that cost 5 and under
 const fiveOrUnder = (drink) => {
@@ -19,9 +21,22 @@ const evenDrinks = coffeeMenu.filter(onlyEven);
 console.table(evenDrinks);
 
 //print the total if you were to order one of every drink
+const totalCost = coffeeMenu.reduce(
+    (total,coffeeItem) => total + coffeeItem.price,
+    0
+);
+console.log("The total cost of the drinks is $" + totalCost);
 
 //print an array with all the drinks that are seasonal
+const seasonalDrinks = coffeeMenu.reduce(
+    (accumulator,currentValue) => {
+        currentValue.seasonal && accumulator.push(currentValue.name);
+        return accumulator;
+    }, []);
+console.table(seasonalDrinks);
 
 //Print all the seasonal drinks with 
 //the words "with imported beans" after the item name.
 // For example: "affogato with imported beans".
+
+seasonalDrinks.forEach(seasonal => console.log(seasonal + " with imported beans"));
